efb59f005461444b179acb512f6e440dcc7427ef
Daynex <deanr@live.de>
2018-04-28 10:59:19 +0200

    Imporved Hash List

--- hashlist.go
+++ hashlist.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/base64"
+	"errors"
 	"fmt"
 )
 
@@ -17,45 +18,82 @@ type List struct {
 	tail *LNode
 }
 
-func (L *List) Insert(tr string) {
-	list := &LNode{
-		next: L.head,
-		tr:   tr,
-	}
-	if L.head != nil {
-		L.head.prev = list
-	}
-	L.head = list
+type HashList struct {
+	list     *List
+	headHash string
+}
 
-	l := L.head
-	for l.next != nil {
+func NewHashList(data []string) (*HashList, error) {
+
+	hashList, err := BuildHashList(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return hashList, nil
+}
+
+func BuildHashList(data []string) (*HashList, error) {
+
+	if len(data) == 0 {
+		return nil, errors.New("Error: cannot construct tree with no content.")
+	}
+
+	list := &List{
+		head: nil,
+		tail: nil,
+	}
+
+	for _, tr := range data {
+		list = Insert(*list, tr)
+	}
+	return nil, nil
+}
+
+func Insert(list List, tr string) *List {
+
+	if list.head == nil {
+		new := &LNode{
+			prev: nil,
+			next: nil,
+			tr:   hashTransaction(hashTransaction(tr)),
+		}
+		list.head = new
+		list.tail = new
+	} else {
+		new := &LNode{
+			next: list.head,
+			prev: nil,
+			tr:   hashTransaction(hashTransaction(tr) + list.head.tr),
+		}
+		list.head = new
+		new.next.prev = new
+	}
+
+	return &list
+}
+
+func Show(list List) {
+	l := list.head
+	for l != nil {
+		fmt.Printf("%+v -> ", l.tr[0:6])
 		l = l.next
 	}
-	L.tail = l
+	fmt.Println("nil")
 }
 
-func (l *List) Show() {
-	list := l.head
-	for list != nil {
-		fmt.Printf("%+v -> ", list.tr[0:6])
-		list = list.next
+func ShowReverse(list List) {
+	l := list.tail
+	for l != nil {
+		fmt.Printf("%+v -> ", l.tr[0:6])
+		l = l.prev
 	}
-	fmt.Println()
+	fmt.Println("nil")
 }
 
-func (l *List) BuildList(data []string) {
-	for _, tr := range data {
-		tailHash := ""
-		if l.tail != nil {
-			tailHash = l.head.tr
-		}
-
-		l.Insert(hashTransaction(tailHash + hashTransaction(tr)))
-	}
-}
-
-func (l *List) GetTail() string {
-	return l.tail.tr
+func GetTail(hl *HashList) string {
+	return hl.list.tail.tr
 }
 
 func hashTransaction(tr string) string {
