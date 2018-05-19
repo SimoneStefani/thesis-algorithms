21f28589550569c03c8fdbf5077ea270d326e964
Simone Stefani <s.stefani95@gmail.com>
2018-05-19 15:06:39 +0200

    Add tests for mt and fmt

---
+++ structures/mt/merkle_tree_test.go
@@ -0,0 +1,50 @@
+package mt
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestBuildMerkleTreeWithNoElements(t *testing.T) {
+	_, err := NewTree([]string{})
+
+	if err == nil {
+		t.Error("Expected error for empty tree")
+	}
+}
+
+func TestBuildMerkleTreeWithSeveralElements(t *testing.T) {
+	mt, _ := NewTree([]string{"A", "B", "C", "D"})
+	depth := mt.Root.Depth()
+
+	if depth != 3 {
+		t.Error("Expected depth 3, got " + strconv.Itoa(depth))
+	}
+}
+
+func TestBuildUnbalancedMerkleTreeWithSeveralElements(t *testing.T) {
+	mt, _ := NewTree([]string{"A", "B", "C"})
+	depth := mt.Root.Depth()
+
+	if depth != 3 {
+		t.Error("Expected depth 3, got " + strconv.Itoa(depth))
+	}
+}
+
+func TestVerifyMerkleTreeValidTransaction(t *testing.T) {
+	data := []string{"A", "B", "C"}
+	_, _, result, _ := VerifyTransaction("B", data)
+
+	if !result {
+		t.Error("Expected true, got false")
+	}
+}
+
+func TestVerifyMerkleTreeInvalidTransaction(t *testing.T) {
+	data := []string{"A", "B", "C"}
+	_, _, _, err := VerifyTransaction("Z", data)
+
+	if err == nil {
+		t.Error("Invalid verification")
+	}
+}
